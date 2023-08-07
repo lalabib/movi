93a6a790c09970eef1cf7699a2495a91155eb6fd
lalabib <labibla21@gmail.com>
2023-08-07 12:45:48 +0700

    initial commit

---
+++ app/src/main/java/com/latihan/lalabib/movi/ui/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.latihan.lalabib.movi.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.latihan.lalabib.movi.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
